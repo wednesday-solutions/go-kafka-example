514c5257574776b4b7b597c887daeaa631359e20
Chris <christin@wednesday.is>
2022-04-29 18:56:07 +0530

    chore: bump go version to 1.17
    Signed-off-by: Chris <christin@wednesday.is>

--- consumer/pre-commit-setup.sh
+++ consumer/pre-commit-setup.sh
@@ -5,10 +5,10 @@ set -e
 echo "--Installing pre-commit & dependencies---"
 brew install pre-commit
 pre-commit install
-GO111MODULE=off go get github.com/fzipp/gocyclo
-GO111MODULE=off go get golang.org/x/tools/cmd/goimports
-GO111MODULE=off go get -v -u github.com/go-critic/go-critic/cmd/gocritic
-GO111MODULE=off go get -u golang.org/x/lint/golint
+go install github.com/fzipp/gocyclo@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/go-critic/go-critic/cmd/gocritic@latest
+go install golang.org/x/lint/golint@latest
 
 # shellcheck disable=SC2181
 if [ "$?" = "0" ]; then
--- libs/go.mod
+++ libs/go.mod
@@ -1,6 +1,6 @@
 module libs
 
-go 1.14
+go 1.17
 
 require (
 	github.com/labstack/echo v3.3.10+incompatible
